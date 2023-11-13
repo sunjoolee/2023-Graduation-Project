9f3659c029e3f16e1c65050d80a6a1f9b4f7a11e
sunjoolee <sheisthepoem@naver.com>
2023-11-13 18:46:44 +0900

    add firebase, openCV

--- app/src/main/java/com/sunjoolee/ksl_translator_demo/MainActivity.kt
+++ app/src/main/java/com/sunjoolee/ksl_translator_demo/MainActivity.kt
@@ -2,10 +2,15 @@ package com.sunjoolee.ksl_translator_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(OpenCVLoader.initDebug()) Log.d("MainActivity_OpenCV", "Loaded successfully.")
+        else Log.d("MainActivity_OpenCV", "Load failed.")
     }
 }
