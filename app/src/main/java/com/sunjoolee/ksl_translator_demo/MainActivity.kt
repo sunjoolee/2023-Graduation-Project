9eb6752a7365a532a3d76cd3bb25f80848002d91
sunjoolee <sheisthepoem@naver.com>
2023-11-13 17:49:14 +0900

    new project

---
+++ app/src/main/java/com/sunjoolee/ksl_translator_demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sunjoolee.ksl_translator_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
